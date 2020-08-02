b4f5edec5a09dfbdc6890fb358106f141d282979
Surya Sankar <suryashankar.m@gmail.com>
2020-08-02 23:01:34 +0530

    added some more public methods

--- otpstore/otpstore.py
+++ otpstore/otpstore.py
@@ -3,64 +3,118 @@ from toolspy import random_string
 import redis
 import json
 
+
 class OtpStore(object):
 
-	def __init__(
-			self, redis_client=None, redis_host="localhost", 
-			otp_length=8, expiry_seconds=120,
-			generation_attempts=3, verification_attempts=2):
-		if redis_client:
-			self.redis_client = redis_client
-		else:
-			self.redis_client = redis.StrictRedis(host=redis_host)
-		self.otp_length = otp_length
-		self.expiry_seconds = expiry_seconds
-		self.generation_attempts = generation_attempts
-		self.verification_attempts = verification_attempts
+    def __init__(
+            self, redis_client=None, redis_host=None,
+            prefix="$otp:",
+            otp_length=8, expiry_seconds=300,
+            generation_attempts=3, verification_attempts=2):
+        self.initialize(
+            redis_client=redis_client,
+            prefix=prefix,
+            redis_host=redis_host, otp_length=otp_length,
+            expiry_seconds=expiry_seconds,
+            generation_attempts=generation_attempts,
+            verification_attempts=verification_attempts
+        )
 
-	def set(self, key, value):
-		return self.redis_client.set(key, value)
+    def initialize(
+            self, redis_client=None, redis_host=None,
+            prefix="$otp:",
+            otp_length=8, expiry_seconds=300,
+            generation_attempts=3, verification_attempts=2):
+        if redis_client:
+            self.redis_client = redis_client
+        else:
+            if redis_host is None:
+                redis_host = "localhost"
+            self.redis_client = redis.StrictRedis(host=redis_host)
+        self.otp_length = otp_length
+        self.expiry_seconds = expiry_seconds
+        self.generation_attempts = generation_attempts
+        self.verification_attempts = verification_attempts
+        self.prefix = prefix
 
-	def get(self, key):
-		return self.redis_client.get(key)
+    def _with_prefix(self, key):
+        return "{}{}".format(self.prefix, key)
 
-	def has(self, key):
-		return self.get(key) is not None
+    def _set(self, key, value):
+        return self.redis_client.set(self._with_prefix(key), value)
 
-	def generate_otp_dict(self, otp=None):
-		if otp is None:
-			otp = random_string(self.otp_length)
-		return {
-			"otps": [otp],
-			"verifs_left": self.verification_attempts,
-		}
+    def _get(self, key):
+        return self.redis_client.get(self._with_prefix(key))
 
-	def initialize_key(self, key, otp=None):
-		self.set(key, json.dumps(self.generate_otp_dict(otp=otp)))
+    def _expire(self, key, expiry_seconds):
+        self.redis_client.expire(self._with_prefix(key), expiry_seconds)
 
-	def set_otp(self, key, otp=None):
-		otp_dict = self.get(key)
-		if otp_dict:
-			if len(otp_dict['ops']) >= self.generation_attempts:
-				raise Exception(
-					"Only {} otps can be generated within a span of {} seconds".format(
-						self.generation_attempts, self.expiry_seconds))
-			else:
-				otp_dict['otps'].append(self.generate_otp_dict(otp=otp))
-		else:
-			self.initialize_key(key, otp=otp)
+    def _generate_otp_string(self, otp_length=None):
+        if otp_length is None:
+            otp_length = self.otp_length
+        return random_string(otp_length)
 
-	def verify_otp(self, key, otp):
-		otp_dict = self.get(key)
-		if otp_dict:
-			if otp_dict['verifs_left'] <= 0:
-				return (False, "Too many wrong attempts. Try generating otp again after some time.")
-			otp_dict['verifs_left'] -= 1
-			if otp in otp_dict['otps']:
-				return (True, None)
-			return (False, "Invalid OTP.")
-		return (False, "OTP has expired. Try generating the OTP again")
+    def _generate_otp_dict(self, otp=None):
+        if otp is None:
+            otp = random_string(self.otp_length)
+        return {
+            "otps": [otp],
+            "verifs_left": self.verification_attempts,
+        }
 
+    def _generate_otp_dict_json(self, otp=None):
+        return json.dumps(self._generate_otp_dict(otp=otp))
 
+    def keys(self):
+        return self.redis_client.keys("{}*".format(self.prefix))
 
+    def delete(self, key):
+        return self.redis_client.delete(self._with_prefix(key))
 
+    def delete_all(self):
+        keys = self.keys()
+        if len(keys) > 0:
+            self.redis_client.delete(*keys)
+
+    def get_otp_dict(self, key):
+        otpjson = self._get(key)
+        if otpjson:
+            return json.loads(otpjson)
+        return None
+
+    def get_otp(self, key):
+        otp_dict = self.get_otp_dict(key)
+        if otp_dict:
+            return otp_dict['otps'][-1]
+        return None
+
+    def set_otp(self, key, otp=None, expiry_seconds=None):
+        otp_dict = self.get_otp_dict(key)
+        if otp_dict:
+            print("got otp dict as ", otp_dict)
+            if len(otp_dict['otps']) >= self.generation_attempts:
+                raise Exception(
+                    "Only {} otps can be generated within a span of {} seconds".format(
+                        self.generation_attempts, self.expiry_seconds))
+            else:
+                print("in else condition")
+                otp_dict['otps'].append(otp or self._generate_otp_string())
+                self._set(key, json.dumps(otp_dict))
+        else:
+            self._set(key, self._generate_otp_dict_json(otp=otp))
+        if expiry_seconds is None:
+            expiry_seconds = self.expiry_seconds
+        if expiry_seconds:
+            self._expire(key, expiry_seconds)
+        return self.get_otp(key)
+
+    def verify_otp(self, key, otp):
+        otp_dict = self.get_otp_dict(key)
+        if otp_dict:
+            if otp_dict['verifs_left'] <= 0:
+                return (False, "Too many wrong attempts. Try generating otp again after some time.")
+            otp_dict['verifs_left'] -= 1
+            if otp in otp_dict['otps']:
+                return (True, None)
+            return (False, "Invalid OTP.")
+        return (False, "OTP has expired. Try generating the OTP again")
--- tests/test_otpstore.py
+++ tests/test_otpstore.py
@@ -6,18 +6,63 @@ import pytest
 
 
 from otpstore import OtpStore
+from time import sleep
+
+QUICK_EXPIRY_TIMEOUT_SECONDS = 3
 
 
 @pytest.fixture
 def otp_store_with_defaults():
-    return OtpStore()
+    otpstore = OtpStore()
+    yield otpstore
+    otpstore.delete_all()
+
+
+@pytest.fixture
+def otp_store_with_quick_expiry():
+    otpstore = OtpStore(expiry_seconds=QUICK_EXPIRY_TIMEOUT_SECONDS)
+    yield otpstore
+    otpstore.delete_all()
 
 
 def test_otp_generation(otp_store_with_defaults):
-	key = "test@example.com"
-	otp_store_with_defaults.set_otp(key)
-	val = otp_store_with_defaults.get(key)
-	assert "otps" in val
+    key = "test@example.com"
+    otp_store_with_defaults.set_otp(key)
+    val = otp_store_with_defaults.get_otp_dict(key)
+    assert "otps" in val
+
+
+def test_otp_get(otp_store_with_defaults):
+    key = "test1@example.com"
+    otp = "O2179897"
+    otp_store_with_defaults.set_otp(key, otp)
+    assert otp_store_with_defaults.get_otp(key) == otp
+
 
 def test_otp_verification(otp_store_with_defaults):
-	pass
+    key = "test1@example.com"
+    otp = "O21790T7"
+    set_otp = otp_store_with_defaults.set_otp(key, otp)
+    print("set otp is ", set_otp)
+    print("otp dict is ", otp_store_with_defaults.get_otp_dict(key))
+    verified, msg = otp_store_with_defaults.verify_otp(key, otp)
+    print("Message is ", msg)
+    assert verified
+
+
+def test_otp_expiry(otp_store_with_quick_expiry):
+    key = "test2@example.com"
+    otp = "D21790T7"
+    otp_store_with_quick_expiry.set_otp(key, otp)
+    sleep(QUICK_EXPIRY_TIMEOUT_SECONDS + 1)
+    verified, msg = otp_store_with_quick_expiry.verify_otp(key, otp)
+    assert not verified and msg == "OTP has expired. Try generating the OTP again"
+
+
+def test_otp_before_expiry(otp_store_with_quick_expiry):
+    key = "test3@example.com"
+    otp = "A1B2C345"
+    otp_store_with_quick_expiry.set_otp(key, otp)
+    sleep(QUICK_EXPIRY_TIMEOUT_SECONDS - 1)
+    verified, msg = otp_store_with_quick_expiry.verify_otp(key, otp)
+    assert verified
